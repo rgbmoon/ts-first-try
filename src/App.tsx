003ed97601cdd50db3a9be640b2cfe3e231a5562
Sergei <sergeiuniverse@gmial.com>
2021-10-09 00:42:02 +0300

    catched data from inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,81 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import './App.css';
 
 
+// не забыть сделать проверку на нечисло, желателньо прям во время ввода
+const Calculator: React.FunctionComponent = () => {
 
+  const [value_a, setValue] = useState('');
 
+  //Отслеживаем событие
+  const inputHandler = (event: 
+    React.ChangeEvent<HTMLInputElement>) => {
+      setValue(event.target.value);
+    } 
+  
+  //Переводим даные с инпута из строки в число
+  const convertToNumber = (inputStringValue: string) => {
+    const numericValue: number = Number(inputStringValue);
+    return numericValue;
+  }
 
-function App() {
+  
+  //Считаем результат
+  const result = (input: string) => {
+    const a: number = convertToNumber(input);
+    if (isNaN(a)) {
+      console.log('Залупа')
+      return
+    }
+    return a**2;
+  }
+  
+
+  return (
+    <div className="Calculator">
+      <div className="row">
+        <input 
+          type="text" 
+          name="a" 
+          value={result(value_a)} 
+          onChange={inputHandler}
+        /> 
+        
+        * X<sup>2</sup> + 
+        
+        <input 
+          type="text" 
+          name="b"
+        /> 
+        
+        * X + 
+        
+        <input 
+          type="text" 
+          name="c"
+        /> 
+        
+        = 0
+
+      </div>
+      <div className="row">
+        <button>
+          Посчитать
+        </button>
+      </div>
+      <div className="row">
+        <div>X1 = {value_a}</div>
+        <div>X2 = {}</div>
+      </div>
+    </div>
+  );
+}
+
+const App: React.FunctionComponent = () => {
+  
   return (
     <div className="App">
-      <div className="Calculator">
-        <div className="row">
-          <input type="text" /> * X2 + 
-          <input type="text" /> * X + 
-          <input type="text" /> = {0}
-        </div>
-        <div className="second-row">
-          <button>Посчитать</button>
-        </div>
-      </div>
+      <Calculator />
     </div>
   );
 }
