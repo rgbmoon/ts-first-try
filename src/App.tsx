0cd15a57f97e6e630d00d87f1e0de1b08a64dafc
Sergei <sergeiuniverse@gmial.com>
2021-10-12 10:06:09 +0300

    Прописал состояние инпутов в родителя

--- src/App.tsx
+++ src/App.tsx
@@ -2,61 +2,66 @@ import React, { useState, useEffect, useMemo } from 'react';
 import './App.css';
 
 
-// не забыть сделать проверку на нечисло, желателньо прям во время ввода
-const Calculator: React.FunctionComponent = () => {
+/*
+3) Посчитать уравнение и вывести X
+*/
 
-  const [value_a, setValue] = useState('');
+const Input = (props: AppProps) => {
+  return(
+    <input 
+      type="text" 
+      name={props.name} 
+      value={props.value}
+      onChange={props.onChange}
+    />
+  );
+}
 
-  //Отслеживаем событие
-  const inputHandler = (event: 
-    React.ChangeEvent<HTMLInputElement>) => {
-      setValue(event.target.value);
+
+const Calculator = () => {
+
+  const [state, setState] = useState({
+    a: '',
+    b: '',
+    c: '',
+  });
+
+  //Отслеживаем ввод
+  const inputHandler = (event: any) => {
+    const { name, value } = event.target;
+    setState(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
     } 
-  
-  //Переводим даные с инпута из строки в число
-  const convertToNumber = (inputStringValue: string) => {
-    const numericValue: number = Number(inputStringValue);
-    return numericValue;
-  }
 
-  
-  //Считаем результат
-  const result = (input: string) => {
-    const a: number = convertToNumber(input);
-    if (isNaN(a)) {
-      console.log('Залупа')
-      return
-    }
-    return a**2;
-  }
+
+  // const prepearingData = (input: string) => {
+
+  //   const num: number = Number(input); // Перевод данных инпута в число
+
+  //   if (isNaN(num)) { // Проверка и отмена ввода невалидных символов
+  //       setState('');
+  //   }
+  //   return;
+  // }
   
 
   return (
     <div className="Calculator">
       <div className="row">
-        <input 
-          type="text" 
-          name="a" 
-          value={result(value_a)} 
-          onChange={inputHandler}
-        /> 
-        
-        * X<sup>2</sup> + 
-        
-        <input 
-          type="text" 
-          name="b"
-        /> 
-        
-        * X + 
-        
-        <input 
-          type="text" 
-          name="c"
-        /> 
-        
-        = 0
-
+        <Input 
+          name='a'
+          value={state.a} 
+          onChange={inputHandler}/> * X<sup>2</sup> +
+        <Input 
+          name='b'
+          value={state.b} 
+          onChange={inputHandler}/> * X +
+        <Input 
+          name='c'
+          value={state.c} 
+          onChange={inputHandler}/> = 0
       </div>
       <div className="row">
         <button>
@@ -64,7 +69,7 @@ const Calculator: React.FunctionComponent = () => {
         </button>
       </div>
       <div className="row">
-        <div>X1 = {value_a}</div>
+        <div>X1 = {[state.a, state.b, state.c]}</div>
         <div>X2 = {}</div>
       </div>
     </div>
@@ -81,3 +86,12 @@ const App: React.FunctionComponent = () => {
 }
 
 export default App;
+
+
+
+
+type AppProps = {
+  onChange: any;
+  value: string;
+  name: string;
+};
